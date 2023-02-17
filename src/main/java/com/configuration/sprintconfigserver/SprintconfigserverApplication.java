bb9cc8bf3c618d8362f07084823d2b1899a6267e
Leon Andres Betancur Herrera <leon.betancur@globant.com>
2023-02-17 09:51:43 -0500

    initial commit

---
+++ src/main/java/com/configuration/sprintconfigserver/SprintconfigserverApplication.java
@@ -0,0 +1,13 @@
+package com.configuration.sprintconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintconfigserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintconfigserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/configuration/sprintconfigserver/SprintconfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.configuration.sprintconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintconfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
