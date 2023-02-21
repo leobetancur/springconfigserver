fee9273dbc728a01d384bcbb8ee0124e7e305b31
Leon Andres Betancur Herrera <leon.betancur@globant.com>
2023-02-21 09:34:43 -0500

    add annotation in order to run configuration server

--- src/main/java/com/configuration/sprintconfigserver/SprintconfigserverApplication.java
+++ src/main/java/com/configuration/sprintconfigserver/SprintconfigserverApplication.java
@@ -2,8 +2,10 @@ package com.configuration.sprintconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class SprintconfigserverApplication {
 
 	public static void main(String[] args) {
